09f8c374b809d7ba466262fcf2f4c45029b374ea
tomer-cohen <tomerc89@gmail.com>
2018-11-20 14:42:11 +0200

    1st commit

---
+++ Webpage.Change.Watcher/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WebpageChangeWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.76923F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(208, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start monitoring";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.76923F);
+            this.button2.Location = new System.Drawing.Point(207, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop monitoring";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.07692F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "   ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.07692F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(200, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "  ";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11.07692F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(204, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "  ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(210, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Check Now";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Aharoni", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(153, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(157, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Aharoni", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(154, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Checking type:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Aharoni", 10.86792F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(157, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "String to search:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F);
+            this.textBox2.Location = new System.Drawing.Point(160, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "String",
+            "ContentLength"});
+            this.comboBox1.Location = new System.Drawing.Point(157, 143);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 483);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Webpage Change Checker";
+            this.Resize += new System.EventHandler(this.form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Webpage.Change.Watcher/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+
+namespace WebpageChangeWatcher
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer1;
+        private string urlForCheck = null;
+        NotifyIcon mynotifyicon = new NotifyIcon();
+        private int optionID;
+        private string stringToCheck = null;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            label6.Hide();
+            textBox2.Hide();
+            button1.Left = (this.ClientSize.Width - button1.Width) / 2; ;
+            button2.Hide();
+            button3.Hide();
+            button2.Left = (this.ClientSize.Width - button2.Width) / 2;
+            button3.Left = (this.ClientSize.Width - button3.Width) / 2;
+            label1.Left = button2.Left - 20;
+            label2.Left = button2.Left - 20;
+            label3.Left = button2.Left - 20;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            urlForCheck = textBox1.Text;
+            string dialogMessage;
+            DialogResult dialogResult = DialogResult.No;
+
+            if (Uri.IsWellFormedUriString(urlForCheck, UriKind.Absolute))
+            {
+                if (optionID == 1)
+                {
+                    if (textBox2.Text.Length > 5)
+                    {
+                        stringToCheck = textBox2.Text;
+                        dialogMessage = string.Format("This will create a monitor for:\r\n{0} \r\n Are you sure?", urlForCheck);
+                        dialogResult = MessageBox.Show(dialogMessage, "Settings Confirmation", MessageBoxButtons.YesNo);
+                    }
+                    else
+                    {
+                        dialogMessage = "String to search is too short!";
+                        dialogResult = MessageBox.Show(dialogMessage, "Settings Confirmation", MessageBoxButtons.YesNo);
+                        return;
+                    } 
+                }
+                else if (optionID == 2)
+                {
+                    dialogMessage = string.Format("This will create a monitor for:\r\n{0} \r\n Are you sure?", urlForCheck);
+                    dialogResult = MessageBox.Show(dialogMessage, "Settings Confirmation", MessageBoxButtons.YesNo);
+                }
+                
+            }
+            else
+            {
+                dialogMessage = "Illegal URL format!";
+                MessageBox.Show(dialogMessage, "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                InitTimer();
+                label1.ForeColor = Color.Green;
+                label1.Text = "Monitoring has been started.";
+                UpdateTimingLabel();
+                label4.Hide();
+                textBox1.Hide();
+                button3.Show();
+                button1.Hide();
+                button2.Show();
+                label5.Hide();
+                label6.Hide();
+                comboBox1.Hide();
+                textBox2.Hide();
+                timer1_Tick(sender, e);
+            }
+            else 
+            {
+                return;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1_Tick(sender, e);
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            Random randTimeInMinutes = new Random();
+            timer1.Interval = randTimeInMinutes.Next(45, 60)*60*1000; //Interval is in miliseconds. 3600000 for 1 hour
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckURL_ForUpdate(urlForCheck, optionID);
+        }
+
+
+        private void StopTimer()
+        {
+            if (timer1 != null)
+            {
+                timer1.Dispose();
+                label1.ForeColor = Color.Red;
+                label1.Text = "Monitoring has been stopped.";
+                label2.Text = "";
+                label3.Text = "";
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Try starting the monitoring first ;)";
+                return;
+            }
+        }
+
+        private void UpdateTimingLabel()
+        {
+            label2.Text = "Last check:\r\n" + DateTime.Now + ".";
+            if (timer1 != null)
+            {
+                label2.Text += "\r\n\r\nNext check cycle will be at:\r\n" + DateTime.Now.AddMilliseconds(timer1.Interval);
+            }
+            
+        }
+
+        private void CheckURL_ForUpdate(string url, int optionID)
+        {
+            UpdateTimingLabel();
+            switch (optionID) {
+                case 1:
+                    checkByString(stringToCheck, url);
+                    break;
+                case 2:
+                    checkByContentLength();
+                    break;
+            }
+
+            return;
+        }
+
+        private void checkByString(string stringToCheck, string url)
+        {
+            WebClient client = new WebClient();
+            string downloadString = client.DownloadString(url);
+
+            if (!(downloadString.Contains(stringToCheck)))
+            {
+                MessageBox.Show("Item is back in stock!");
+                label3.BackColor = Color.Black;
+                label3.ForeColor = Color.LawnGreen;
+                label3.Text = "***Item is back in stock***";
+                //sendSuccessEmail("tomer.cohen@buzzr.biz");
+                return;
+            }
+            else
+            {
+                label3.ForeColor = Color.Firebrick;
+                label3.Text = "Item is still out of stock.";
+                return;
+            }
+        }
+
+        private void checkByContentLength()
+        {
+
+        }
+
+        public bool sendSuccessEmail(string recipient)
+        {
+
+            try
+            {
+                MailMessage mail = new MailMessage("reports@buzzr.biz", recipient);
+                SmtpClient mailClient = new SmtpClient();
+                mailClient.Port = 587; //for gmail: 587
+                mailClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                mailClient.UseDefaultCredentials = false;
+                mailClient.Host = "za-smtp-outbound-1.mimecast.co.za";
+                mailClient.Credentials = new System.Net.NetworkCredential("ramint\reports", "R3ports123");
+                mail.Subject = "This is a test email subject";
+                mail.Body = "This is a test email body";
+                mailClient.Send(mail);
+                return true;
+            }
+            
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+
+        private void form1_Resize(object sender, EventArgs e)
+        {            
+            mynotifyicon.Click += new System.EventHandler(showWindow);
+            mynotifyicon.Icon = new Icon(@"../../Oxygen-Icons.org-Oxygen-Places-certificate-server.ico");
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                mynotifyicon.Visible = true;
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                mynotifyicon.Visible = false;
+            }
+
+        }
+
+        private void showWindow(object sender, System.EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            mynotifyicon.Visible = false;
+        }
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+      {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            var selectedItem = comboBox1.SelectedItem.ToString();
+            if (selectedItem == "String")
+            {
+                optionID = 1;
+                label6.Show();
+                textBox2.Show();
+            }
+
+            else if (selectedItem == "ContentLength")
+            {
+                optionID = 2;
+                label6.Hide();
+                textBox2.Hide();
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+
+
+
+    }
+}
